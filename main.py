9a16d2d57ed3f7f0cc9e9cda08fdd721a5a85d73
Samuel Ytterbrink <neppord@gmail.com>
2012-06-06 18:48:04 +0200

    tests passes

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ def lexer(string):
     [1]
     >>> lexer('2')
     [2]
+    >>> lexer('1.5')
+    [1.5]
     >>> lexer('1 + 2')
     [1, '+', 2]
     """
     return [
-        int(token) if token.isdigit() else token for token in string.split()
+        int(token) if token.isdigit() else
+        float(token) if '.' in token else token
+        for token in string.split()
     ]
